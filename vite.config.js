dde609b7b15c4730fdbd9970d0844b303a3ccbc9
hsl0 <46296273+hsl0@users.noreply.github.com>
2026-04-17 05:40:29 +0900

    feat: initialize Vue project with Vite and add frontend build configuration
    - Added package.json for project dependencies and scripts.
    - Integrated frontend-maven-plugin in pom.xml for Node.js and npm management.
    - Created main.js to define custom elements from Vue components.
    - Configured Vite for building and serving the application with appropriate settings.

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => ({
+  plugins: [vue(), vueDevTools()],
+  root: fileURLToPath(new URL("./src/main/resources/templates", import.meta.url)),
+  build: {
+    minify: mode !== "development",
+    sourcemap: mode !== "production",
+    outDir: fileURLToPath(new URL("./target/classes/static/assets", import.meta.url)),
+    rolldownOptions: {
+      input: [
+        fileURLToPath(new URL("./src/main/js/assets/main.js", import.meta.url)),
+        fileURLToPath(new URL("./src/main/js/assets/main.css", import.meta.url)),
+      ],
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
+      },
+    },
+  },
+  publicDir: fileURLToPath(new URL("./src/main/resources/static", import.meta.url)),
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src/main/js", import.meta.url)),
+    },
+  },
+  define: {
+    "process.env.NODE_ENV": JSON.stringify(mode),
+    __VUE_PROD_DEVTOOLS__: mode === "development",
+  },
+}));
