061cc80b5232af74fd3a7fb18673f9aad65263e6
hsl0 <46296273+hsl0@users.noreply.github.com>
2026-04-18 00:55:35 +0900

    feat: add vite-plugin-vue-ce for custom element support and update configuration

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
+import vueCustomElements from "vite-plugin-vue-ce";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [vue(), vueDevTools()],
-  root: fileURLToPath(new URL("./src/main/resources/templates", import.meta.url)),
+  plugins: [vue(), vueCustomElements(), vueDevTools()],
+  root: fileURLToPath(new URL("./src/main/resources/", import.meta.url)),
   build: {
     minify: mode !== "development",
     sourcemap: mode !== "production",
