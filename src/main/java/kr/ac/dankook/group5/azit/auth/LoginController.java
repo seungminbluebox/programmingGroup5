c627be65a5d216884544061338c3464d48acf284
hsl0 <46296273+hsl0@users.noreply.github.com>
2026-06-09 17:27:43 +0900

    feat: 로그인/회원가입 모달 팝업 기능 추가

--- src/main/java/kr/ac/dankook/group5/azit/auth/LoginController.java
+++ src/main/java/kr/ac/dankook/group5/azit/auth/LoginController.java
@@ -1,13 +1,15 @@
 package kr.ac.dankook.group5.azit.auth;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String login() {
+    public String login(Model model) {
+        model.addAttribute("isModal", false);
         return "login";
     }
 }
--- src/main/java/kr/ac/dankook/group5/azit/auth/RegisterController.java
+++ src/main/java/kr/ac/dankook/group5/azit/auth/RegisterController.java
@@ -3,6 +3,7 @@ package kr.ac.dankook.group5.azit.auth;
 import kr.ac.dankook.group5.azit.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +14,8 @@ public class RegisterController {
     private final UserService userService;
 
     @GetMapping("/register")
-    public String registerForm() {
+    public String registerForm(Model model) {
+        model.addAttribute("isModal", false);
         return "register";
     }
 
